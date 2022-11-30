db700b2ac57c2a8933e0945b0c280303e8cda92c
madhutammisetti <85747431+madhutammisetti@users.noreply.github.com>
2022-11-30 23:42:51 +0530

    Added pratice problems
    added pratice problems on connecting to dataserver

--- app.js
+++ app.js
@@ -106,3 +106,125 @@ to server and template engines(ejs,) */
 // app.listen(8080,()=>{
 //     console.log("Server satrted runnning on port 8080")
 // })
+
+
+/***************************************************************************** */
+
+/***********Render Video with the help of streams**********/
+
+// const fs = require("fs")
+// const http = require("http");
+
+// const file = "./video.mp4";
+
+// const {createReadStream} = require("fs")
+
+// const server = http.createServer( (req,res)=>{
+//     res.writrHead(200,{'content-Type':'video/mp4'})
+//     createReadStream(file).pipe(res)
+// })
+
+/***************************************************************************** */
+
+/***********Pratice Problems**********/
+/*Create a table in mysql , insert data init and create any get route and display data
+which is stored in the database */
+
+// const express = require("express");
+// const app = express();
+// const mysql = require("mysql")
+
+// const db = mysql.createConnection({
+//     host:"localhost",
+//     user:"root",
+//     password:"071436"
+// })
+
+// db.connect((err)=>{
+//     if(err){
+//         console.log(err)
+//     }else{
+//         console.log("DB connected")
+//     }
+// })
+
+// app.get("/countries" , (req,res)=>{
+//     let sqlquery = 'SELECT * FROM world.country;'
+//     db.query(sqlquery,(err,data)=>{
+//         if(err){
+//             res.send(err)
+//         }else{
+//             res.send(data)
+//         }
+//     })
+// })
+
+// app.listen(8080,()=>{
+//     console.log("Server satrted runnning on port 8080")
+// })
+
+
+/*On calling the get api get the data from database and send ejs file as response
+and display that data dynamically in ejs file */
+
+// const express = require("express");
+// const app = express();
+// const mysql = require("mysql")
+// app.set("viewengine" , "ejs")
+
+// const db = mysql.createConnection({
+//         host:"localhost",
+//         user:"root",
+//         password:"071436"
+//     })
+// db.connect((err)=>{
+//     if(err){
+//         console.log(err)
+//     }else{
+//         console.log("DB connected")
+//     }
+// })
+
+
+// app.get("/countries" , (req,res)=>{
+//     let sqlquery = 'SELECT * FROM world.country;'
+//     db.query(sqlquery,(err,data)=>{
+//         if(err){
+//             res.send(err)
+//         }else{
+//             res.render("countries.ejs",{countryDetails:data})
+//         }
+//     })
+// })
+
+
+// app.listen(8080,()=>{
+//     console.log("Server satrted runnning on port 8080")
+// })
+
+
+
+/*Create a Post route /register , user must send data email,phno,password,confrom password. If password
+and confrom password matches then send response user registered else 
+give a sutiable response with status code */
+
+// const express = require("express");
+// const app = express();
+
+// app.use(express.json())
+
+// //sending data from postman .
+
+// app.post('/',(req,res)=>{
+//     console.log(req.body)
+//     const {password,confrompassword} = req.body;
+//     if(password === confrompassword){
+//         res.json({sucess:"user registred"})
+//     }else{
+//         res.status(417).json({fail:"password not matched"})
+//     }
+// })
+
+// app.listen(8080,()=>{
+//     console.log("Server satrted runnning on port 8080")
+// })
